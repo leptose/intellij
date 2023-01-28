4a5e64b742f8b505a104794a388187cb6e42e0b3
leptose <leptose@gmail.com>
2023-01-28 14:40:57 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hi,Hello");
+        int a=25;
+        int b=10;
+        int c=1;
+    }
+}
